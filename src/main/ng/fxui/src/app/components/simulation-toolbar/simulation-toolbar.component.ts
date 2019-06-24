9b33654129f8581a440734e489cf8516fbbdc5d3
tj562y <tj562y@att.com>
2019-06-23 22:26:37 -0700

    modify default simulation count to be higher add some comments

--- src/main/ng/fxui/src/app/components/simulation-toolbar/simulation-toolbar.component.ts
+++ src/main/ng/fxui/src/app/components/simulation-toolbar/simulation-toolbar.component.ts
@@ -8,7 +8,7 @@ import {TeamService} from "../../services/team.service";
 })
 export class SimulationToolbarComponent implements OnInit {
 
-  simulationCount = 5;
+  simulationCount = 15;
 
   constructor(private teamService: TeamService) {
   }
@@ -16,10 +16,18 @@ export class SimulationToolbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * Kick off the simulation on the backend
+   * @param event - unused
+   */
   private simulateClicked(event): void {
     this.teamService.randomSimulation(this.simulationCount);
   }
 
+  /**
+   * Call the 'reset' endpoint on the backend to make all scores go to zero.
+   * @param event - unused
+   */
   private resetClicked(event): void {
     this.teamService.resetScores();
   }
