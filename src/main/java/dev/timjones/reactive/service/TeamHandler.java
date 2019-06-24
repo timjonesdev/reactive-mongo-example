165ad9fb70bb7ae7fc65433a6e99f47140088d42
tj562y <tj562y@att.com>
2019-06-23 22:36:36 -0700

    minor changes and ++comments

--- src/main/java/dev/timjones/reactive/service/TeamHandler.java
+++ src/main/java/dev/timjones/reactive/service/TeamHandler.java
@@ -23,6 +23,8 @@ import java.util.Random;
 @Component
 public class TeamHandler {
 
+    private Random random;
+
     private TeamRepository teamRepository;
 
     private TeamWatcher teamWatcher;
@@ -31,6 +33,7 @@ public class TeamHandler {
     public TeamHandler(TeamRepository teamRepository, TeamWatcher teamWatcher) {
         this.teamRepository = teamRepository;
         this.teamWatcher = teamWatcher;
+        this.random = new Random();
     }
 
     /**
@@ -109,32 +112,35 @@ public class TeamHandler {
         return ServerResponse.ok().body(this.zeroPlayers(), Team.class);
     }
 
-
     /**
      * Randomize score updates. Helper function to kick off streams to the UI.
      *
-     * @param request
+     * @param request must take the form /update/{count}
      * @return
      */
     public Mono<ServerResponse> randomizeScore(ServerRequest request) {
         String countString = request.pathVariable("count");
         int count = Integer.valueOf(countString);
-        
+
         if (count < 0 || count > 40) {
             return ServerResponse.badRequest().body(BodyInserters.fromObject("Count must be between 0 and 40"));
         }
 
+
         Flux<String> playerNames = this.teamRepository.findAll()
                 .map(Team::getPlayers)
-                .map(players -> players.stream()
-                        .map(Player::getName))
+                .map(players -> players.stream().map(Player::getName))
+                // collect lists into a Flux of Strings
                 .flatMap(Flux::fromStream)
+                // convert to a Mono of List<String>
                 .collectList()
                 .map(list -> {
+                    // work with the single unified list
                     while (list.size() < count) {
                         // Double the size of the list until it's bigger than the count
                         list.addAll(list);
                     }
+                    // randomize by shuffling order
                     Collections.shuffle(list);
                     return list;
                 })
@@ -144,6 +150,7 @@ public class TeamHandler {
                 .map(pulse -> this.randomDouble())
                 .take(count);
 
+        // combine the two fluxes and update player scores with random Doubles
         Flux<Team> updateFlux = Flux.zip(doubleFlux, playerNames)
                 .flatMap(objects -> {
                     Double scoreChange = objects.getT1();
@@ -166,16 +173,22 @@ public class TeamHandler {
                 .sum();
     }
 
+    /**
+     * Helper method to randomly grab a Double between -2.0 and 10.0
+     *
+     * @return a random double
+     */
     private Double randomDouble() {
-        Random random = new Random();
-        return random.doubles(1L, -2.0, 10.0).sum();
+        return this.random.doubles(1L, -2.0, 10.0).sum();
     }
 
-    private Integer randomInteger(int max) {
-        Random random = new Random();
-        return random.nextInt(max);
-    }
-
+    /**
+     * Update the score of a player by name, and update the player's team total score
+     *
+     * @param playerName  the name of the player to update
+     * @param scoreChange the difference in score to apply
+     * @return the updated Team
+     */
     private Mono<Team> updateTeam(String playerName, Double scoreChange) {
         log.info("Player: " + playerName + ", Score Change: " + scoreChange);
         return this.teamRepository.findDistinctByPlayerName(playerName)
@@ -194,6 +207,11 @@ public class TeamHandler {
                 .flatMap(teamRepository::save);
     }
 
+    /**
+     * Set all player scores to zero
+     *
+     * @return a Flux of updated teams
+     */
     private Flux<Team> zeroPlayers() {
         return this.teamRepository.findAll()
                 .map(team -> {
